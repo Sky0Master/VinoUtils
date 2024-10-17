5ccf6395f15dc45f0aa913a5749752862baf2775
Vino <sky0master@163.com>
2024-10-17 14:52:37 +0800

    object pool

---
+++ source/ObjectPool/GenericObject.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace VinoUtils.ObjectPool 
+{
+public class GenericObject : MonoBehaviour, IPooledObject
+{
+    public float timeToDestroy = 3f;
+    public float countDownTimer = 3f;
+    public string poolTag;
+    public bool isActive = false;
+
+    public void OnRequestedFromPool()
+    {
+        isActive = true;
+        countDownTimer = timeToDestroy;
+    }
+
+    public void DiscardToPool()
+    {
+        MyPooler.ObjectPooler.Instance.ReturnToPool(poolTag, this.gameObject);
+        isActive = false;
+    }
+
+    void Update()
+    {
+        if (isActive)
+        {
+            countDownTimer -= Time.deltaTime;
+            if (countDownTimer <= 0f)
+            {
+                DiscardToPool();
+            }
+        }
+    }
+}
+}
---
+++ source/ObjectPool/IPooledObject.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace VinoUtils.ObjectPool
+{
+	public interface IPooledObject
+	{
+		void OnRequestedFromPool();
+		void DiscardToPool();
+	}
+}
+
+
+
