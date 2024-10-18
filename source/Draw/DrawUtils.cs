de0c3f2940b0875c68bf2bb77f09b17e689fb9af
Vino <sky0master@163.com>
2024-10-18 18:26:38 +0800

    Draw Text

--- source/Draw/DrawUtils.cs
+++ source/Draw/DrawUtils.cs
@@ -37,14 +37,25 @@ public static class DrawUtils
         newCircle.fillColor = color;
         Circle.Draw(newCircle);
     }
-    // public static void DrawCenterText(string content, float existTime = 1f, float fadeDuration = 1f)
-    // {
-    //     var go = GameObject.Instantiate(Resources.Load("FloatingText"));
-    //     var fade = go.GetComponent<UICanvasGroupFade>();
-    //     fade.fadeDelay = existTime;
-    //     fade.duration = fadeDuration;
-    //     var tmp = go.GetComponentInChildren<TextMeshProUGUI>();
-    //     tmp.text = content;
-    // }
+    public static void DrawCenterText(string content, float existTime = 1f, float fadeDuration = 1f,float offsetX = 0f, float offsetY = 0f)
+    {
+        var go = GameObject.Instantiate(Resources.Load("FloatingText"));
+        var fade = go.GetComponent<UICanvasGroupFade>();
+        fade.fadeDelay = existTime;
+        fade.duration = fadeDuration;
+        var tmp = go.GetComponentInChildren<TextMeshProUGUI>();
+        tmp.text = content;
+        tmp.rectTransform.anchoredPosition = new Vector2(offsetX, offsetY);
+    }
+    public static void DrawBottomText(string content, float existTime = 1f, float fadeDuration = 1f,float offsetX = 0f, float offsetY = 0f)
+    {
+        var go = GameObject.Instantiate(Resources.Load("FloatingTextBottom"));
+        var fade = go.GetComponent<UICanvasGroupFade>();
+        fade.fadeDelay = existTime;
+        fade.duration = fadeDuration;
+        var tmp = go.GetComponentInChildren<TextMeshProUGUI>();
+        tmp.text = content;
+        tmp.rectTransform.anchoredPosition = new Vector2(offsetX, offsetY);
+    }
 }
 }
