09edb917ef63350dd30e29c3a916464cb3d12efd
Vino <sky0master@163.com>
2024-10-30 16:36:25 +0800

    Fix bug

--- source/DontDestroyChildren.cs
+++ source/DontDestroyChildren.cs
@@ -1,12 +1,8 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using VinoUtility;
-
 public class DontDestroyChildren : MonoBehaviour
 {
    private void Awake() {
-      var go = GameManager.FindObjectOfType<DontDestroyChildren>();
+      var go = FindObjectOfType<DontDestroyChildren>();
       if(go != this) 
       {
          Destroy(gameObject);
